defb2960f0c1a973259b4746ee722f445ebefca9
Daviribs <davirb21@gmail.com>
2025-11-27 12:44:41 -0300

    update pagewrapperroot and netxauth

--- src/components/PageWrapper/PageWrapperRoot.tsx
+++ src/components/PageWrapper/PageWrapperRoot.tsx
@@ -1,6 +1,6 @@
 import requestApi from "@/helpers/requestApi"
 import { useRouter } from "next/navigation"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 interface PageWrapperRootProps {
   children: React.ReactNode
@@ -12,30 +12,32 @@ export default function PageWrapperRoot({
   withAuth = false,
 }: PageWrapperRootProps) {
   const router = useRouter()
-  useEffect(() => {
-    if (!withAuth) {
-      return
-    }
-    const token = localStorage.getItem("token")
-    if (!token) {
-      router.push("/login")
-    }
 
-    async function heartBeat() {
-      try {
-        const response = await requestApi({
-          url: "/profile",
-          method: "GET",
-        })
-        localStorage.setItem("user", JSON.stringify(response.data))
-      } catch (error) {
-        localStorage.removeItem("token")
-        localStorage.removeItem("user")
-        console.error(error)
+  useEffect(() => {
+    const user = localStorage.getItem("user")
+    const token = localStorage.getItem("token")
+
+    if (withAuth || (user && token)) {
+      if (!token) {
         router.push("/login")
       }
+
+      async function heartBeat() {
+        try {
+          const response = await requestApi({
+            url: "/profile",
+            method: "GET",
+          })
+          localStorage.setItem("user", JSON.stringify(response.data))
+        } catch (error) {
+          localStorage.removeItem("token")
+          localStorage.removeItem("user")
+          console.error(error)
+          router.push("/login")
+        }
+      }
+      heartBeat()
     }
-    heartBeat()
   }, [])
 
   return <div className="min-h-screen bg-[#111418]">{children}</div>
